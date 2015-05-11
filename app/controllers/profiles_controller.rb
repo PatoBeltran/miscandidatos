797bf59c15ef8b3acf4681d2ad6a556c4ca4b595
PatoBeltran <pbelsal@gmail.com>
2015-05-11 17:42:31 -0500

    order candidates by name ascending

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,19 +6,19 @@ class ProfilesController < ApplicationController
 
   def governor
     @areas = CandidateArea.all
-    @candidates = Candidate.where(candidate_area_id: CandidateArea.where(name: "Gubernatura").pluck(:id).first, active: true)
+    @candidates = Candidate.where(candidate_area_id: CandidateArea.where(name: "Gubernatura").pluck(:id).first, active: true).order("name ASC")
     @filters = PoliticalParty.where("id IN (?)", @candidates.pluck(:political_party_id))
   end
 
   def deputy
     @areas = CandidateArea.all
-    @candidates = Candidate.where(candidate_area_id: CandidateArea.where(name: "Diputación").pluck(:id).first, active: true)
+    @candidates = Candidate.where(candidate_area_id: CandidateArea.where(name: "Diputación").pluck(:id).first, active: true).order("name ASC")
     @filters = GeographicalArea.where("id IN (?)", @candidates.pluck(:geographical_area_id))
   end
 
   def mayor
     @areas = CandidateArea.all
-    @candidates = Candidate.where(candidate_area_id: CandidateArea.where(name: "Alcaldía").pluck(:id).first, active: true)
+    @candidates = Candidate.where(candidate_area_id: CandidateArea.where(name: "Alcaldía").pluck(:id).first, active: true).order("name ASC")
     @filters =  GeographicalArea.where("id IN (?)", @candidates.pluck(:geographical_area_id))
   end
 end
