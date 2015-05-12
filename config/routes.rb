f5006d201b020d2e3e3707f8ae4af4c8e649a437
PatoBeltran <pbelsal@gmail.com>
2015-05-12 00:18:18 -0500

    Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
 
   get '/profiles/governor' => 'profiles#governor'
-  get '/profiles/deputy' => 'profiles#deputy'
   get '/profiles/mayor' => 'profiles#mayor'
 
   get '/legal' => 'pages#legal'
